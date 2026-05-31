223d443636895b3aa25cda6b7a55247498c572f3
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2026-05-31 22:55:41 +0100

    fix: only show "View all" link if there are more than 3 technical writeups

--- src/components/technical-writeups.tsx
+++ src/components/technical-writeups.tsx
@@ -15,12 +15,14 @@ export function TechnicalWriteups() {
 				<h2 className="font-medium text-neutral-950 text-sm dark:text-neutral-50">
 					Technical Writeups
 				</h2>
-				<HapticLink
-					className="text-neutral-500 text-sm underline decoration-neutral-200 underline-offset-4 transition-colors hover:text-neutral-950 hover:decoration-neutral-950 dark:text-neutral-400 dark:decoration-neutral-800 dark:hover:text-neutral-50 dark:hover:decoration-neutral-50"
-					to="/technical-writeups"
-				>
-					View all
-				</HapticLink>
+				{TECHNICAL_WRITEUPS.length > 3 ? (
+					<HapticLink
+						className="text-neutral-500 text-sm underline decoration-neutral-200 underline-offset-4 transition-colors hover:text-neutral-950 hover:decoration-neutral-950 dark:text-neutral-400 dark:decoration-neutral-800 dark:hover:text-neutral-50 dark:hover:decoration-neutral-50"
+						to="/technical-writeups"
+					>
+						View all
+					</HapticLink>
+				) : null}
 			</div>
 			<ItemGroup
 				groupBy="month"
